5e23dd68c04bf463a787eb331081fcbc720a83e3
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-02-22 15:52:20 -0600

    links work

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import {Link } from 'react-router-dom'
 
 const Welcome = () => {
+    // let handleLogin = ()=>{
+
+    //     return <Redirect to='/login'/>
+    // }
     return (
         
-        <div class="welcomeBox">
+        <div className="welcomeBox">
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet"></link>
-           <h1 class="welcomeTo">Welcome to Beach Bums</h1>
-            <p class="welcomePara">Kick back, grab a margarita, turn on some Jimmy Buffet, and follow some people out to the beach.</p>
+           <h1 className="welcomeTo">Welcome to Beach Bums</h1>
+            <p className="welcomePara">Kick back, grab a margarita, turn on some Jimmy Buffet, and follow some people out to the beach.</p>
             <form>
-                <div class="loginButton">
-                    <button class="login">Login</button>
+                <div className="loginButton">
+                    <Link className="login" to='/login'>Log In</Link>
+                    {/* <button className="login">Login</button> */}
                 </div>
-                <p class="account?">Don't have an account?</p>
-                <div class="signupButton">
+                <p className="account?">Don't have an account?</p>
+                <div className="signupButton">
                     
-                    <button class="signup">SignUp</button>
+                    <Link className="signup" to="/signup">SignUp</Link>
                 </div>
             </form>
         </div>
