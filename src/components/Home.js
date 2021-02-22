b984e77ccac1c3b737d2007e72d8ab66127d370f
unknown <drake.tyksinski@live.com>
2021-02-22 17:01:48 -0500

    changed home route, added jsx to render more profile information

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Your Feed Goes here</h1>
+            <p>Post 1</p>
+            <p>Post 2</p>
+            <p>Post 3</p>
+        </div>
+    );
+}
+
+export default Home;
