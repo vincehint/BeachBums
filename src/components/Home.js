9775782160228ef843352caa74805de758ae464a
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-02-23 15:42:36 -0600

    working on layout for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,37 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <form className="postForm">
-                <div>
-                    <label htmlFor="postStatus" className="statusLabel">How You Doin' Dude?</label>
-                </div>
-                <div>
-                    <input className="statusBox" type="text"></input>
-                </div>
-                <div>
-                    <input type="submit"></input>
-                </div>
-            </form>
-            <div className="feed">
-                <div className="posts">
-                    <ul>
-                        <li>
-                            <h3>posts will go here</h3>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <div className="home-wrapper">
+            
+
+
+            
+                        <form className="postForm">
+                            <div>
+                                <label htmlFor="postStatus" className="statusLabel">How You Doin' Dude?</label>
+                            </div>
+                            <div>
+                                <input className="statusBox" type="text"></input>
+                            </div>
+                            <div>
+                                <input type="submit"></input>
+                            </div>
+                        </form>
+                   
+                            <ul>
+                                <li>I Follow This Person</li>
+                                <li>I Follow This Person</li>
+                                <li>I Follow This Person</li>
+                                <li>I Follow This Person</li>
+                            </ul>
+                        
+                            <ul>
+                                <li>
+                                    <h3>posts will go here</h3>
+                                </li>
+                            </ul>
+                        
+        </div>   
     );
 }
 
